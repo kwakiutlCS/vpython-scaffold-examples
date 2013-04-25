aba04d7d08167597963b3b5ad27690a644ad1b54
ricardo <ricardo.rsr.rodrigues@gmail.com>
2013-04-25 12:09:46 +0100

    minor changes

--- simple-movement/simple-movement.py
+++ simple-movement/simple-movement.py
@@ -5,7 +5,10 @@ import math
 
 ## background color
 scene.background = (1,1,1)
-scene.foreground = (0,1,1)
+
+scene.autoscale = False
+scene.range = (30,30,30)
+
 print "\n\n"
 
 
@@ -16,32 +19,8 @@ print "\n\n"
 #                                                                #
 ##################################################################
 
-# projectile (it's a ball), sets its initial position as pos = (x,y,z)
-ball = sphere(color = color.red, pos = (0,0,0), make_trail= True, trail_type="points", interval=10, retain=50)
-
-
-##################################################################
-#                PHYSICS ENDS                                    #
-##################################################################
-scene.autoscale = False
-scene.range = (30,30,30)
-
-##################################################################
-#                                                                #
-#                PHYSICS HERE                                    #
-#                                                                #
-##################################################################
-
-# projectile characteristics
-ball.mass = 1 # kg
-ball.velocity = vector(-4,0,0) # m/s
-
-# sets up a timer
-timer = 0.00 #s
-
-# timer increment
-dt = 0.01 #s
-
+# body (it's a ball), sets its initial position as pos = (x,y,z)
+ball = sphere(pos = (0,0,0), color = color.red, make_trail= True, trail_type="points", interval=10, retain=50)
 
 
 # starts the animation
@@ -50,18 +29,7 @@ while True:
     rate(100)
    
     # updates the projectile position 
-    ball.pos = ball.pos + ball.velocity*dt
+    ball.pos = vector(0,0,0)
     
-    # sets the total force
-    F = vector(2,0,0) 
-
-    # calculates the projectile acceleration
-    ball.accel = F / ball.mass 
-    
-    # updates the projectile velocity
-    ball.velocity = ball.velocity + ball.accel * dt
-
-    # updates the timer
-    timer = timer + dt
     
     
